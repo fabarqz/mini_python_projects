0fd4a92f4de13cf2b0ed2a2fb35a2d2a95949314
franz abarquez <franzdale.abarquez@gmail.com>
2024-05-07 17:25:45 +0800

    updated basic_dice project

--- basic_dice/main.py
+++ basic_dice/main.py
@@ -7,13 +7,28 @@ class Dice:
         AVAILABLE_DICE_TYPES = {4, 6, 8, 10, 12, 20}
         while True:
             try:
-                dice_choice = int(input("What dice type would you like to roll: "))
+                dice_choice = int(input("What dice type would you like to roll? "))
                 if dice_choice not in AVAILABLE_DICE_TYPES:
                     raise ValueError("Invalid dice type")
 
                 return dice_choice
             except ValueError as e:
                 print(f"Error. {e}. Try again")
+
+    @staticmethod
+    def getRolls() -> int:
+        number_rolls = int(input("How many times would you like to roll the dice? "))
+        return number_rolls
+
+    @staticmethod
+    def multiple_rolls(dice_choice, number_rolls) -> list:
+        results = []
+
+        for i in range(number_rolls):
+            roll_result = Dice.roll(dice_choice)
+            results.append(roll_result)
+
+        return results
 
     @staticmethod
     def roll(dice_type) -> int:
@@ -24,5 +39,8 @@ class Dice:
 if __name__ == "__main__":
     dice_instance = Dice()
     current_dice = dice_instance.getDiceType()
-    roll_result = dice_instance.roll(current_dice)
-    print(roll_result)
+    number_rolls = dice_instance.getRolls()
+    results = dice_instance.multiple_rolls(current_dice, number_rolls)
+
+    # roll_result = dice_instance.roll(current_dice)
+    print(results)
