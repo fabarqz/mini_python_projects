30c3a3582576c012a0e048467196d843bc05545f
franz abarquez <franzdale.abarquez@gmail.com>
2024-06-09 22:27:44 +0800

    updated fancy_line_print

--- fancy_line_print/main.py
+++ fancy_line_print/main.py
@@ -13,12 +13,25 @@ def fancy_print(x: str) -> str:
     pass
 
 
-def is_enter(key):
-    if key == keyboard.Key.enter:
-        return False
+def typingEffect(text):
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        sleep(0.05)
+
+
+def wait_for_enter():
+    def on_press(key):
+        if key == keyboard.Key.enter:
+            return False  # Stop the listener
+
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
 
 
 if __name__ == "__main__":
-    print("This is a test text. Follow the instruction on the next line")
+    typingEffect("This is a test text. Follow the instruction on the next line\n")
+    typingEffect("Press enter to continue\n")
+    wait_for_enter()
     delete_last_line()
-    print("The last line should have been deleted")
+    typingEffect("The last line should have been deleted\n")
