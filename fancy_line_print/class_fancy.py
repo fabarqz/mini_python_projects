7f696a21c8e104271f05e8f95b32a429e673201a
franz abarquez <franzdale.abarquez@gmail.com>
2024-06-11 14:25:42 +0800

    committed new file under fancy_line_print

---
+++ fancy_line_print/class_fancy.py
@@ -0,0 +1,36 @@
+from sys import stdout
+from time import sleep
+from pynput import keyboard as board
+import keyboard
+
+
+class Fancy:
+    def __init__(self):
+        self.number_of_lines = 0
+
+    def delete_one_line(self):
+        stdout.write("\x1b[1A")
+        stdout.write("\x1b[2k")
+        self.typingEffect(" " * 50)
+
+    def typingEffect(self, text):
+        for character in text:
+            stdout.write(character)
+            stdout.flush()
+            sleep(0.05)
+
+    def printLine(self, text):
+        self.number_of_lines += 1
+        return self.typingEffect(text)
+
+
+if __name__ == "__main__":
+    fancy = Fancy()
+    fancy.printLine("This is a testLine\n")
+    sleep(1)
+    fancy.printLine("This is a another line\n")
+    sleep(1)
+    fancy.printLine("This third line will soon be deleted\n")
+    sleep(1)
+    fancy.delete_one_line()
+    fancy.printLine("\nThis a new line")
