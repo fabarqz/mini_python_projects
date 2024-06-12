09892c357290216b0eb671de9a742255e5f07eaf
franz abarquez <franzdale.abarquez@gmail.com>
2024-06-12 13:49:52 +0800

    added new methods for class_fancy

--- fancy_line_print/README.md
+++ fancy_line_print/README.md
@@ -1,3 +1,5 @@
 This is an incomplete program experimenting with ways of presenting text in the terminal.
 
 As it is the code is in a broken state. Will revert back to this once I have further inspiration on what I want to achieve
+
+At the current state this project serves as a sandbox of sorts for developing various ways of manipulating how text is presented in the terminal.
--- fancy_line_print/class_fancy.py
+++ fancy_line_print/class_fancy.py
@@ -1,17 +1,32 @@
+from os import system
 from sys import stdout
 from time import sleep
 from pynput import keyboard as board
 import keyboard
+import pyfiglet
 
 
 class Fancy:
     def __init__(self):
         self.number_of_lines = 0
 
+    def get_name(self):
+        self.printLine("Please enter your name: ")
+        self.name = input()
+        fancy.delete_one_line()
+        fancy.printLine(f"\rWelcome, {fancy.name}")
+
     def delete_one_line(self):
         stdout.write("\x1b[1A")
         stdout.write("\x1b[2k")
         self.typingEffect(" " * 50)
+        stdout.write("\x1b[1A")
+        print("\r")
+
+    def replace_previous_line(self, text):
+        stdout.write("\x1b[1A")
+        stdout.write("\r\n")
+        self.printLine(text)
 
     def replace_line(self, text):
         stdout.write("\x1b[1A")
@@ -27,15 +42,27 @@ class Fancy:
         self.number_of_lines += 1
         return self.typingEffect(text)
 
+    def print_banner(self):
+        self.banner = pyfiglet.figlet_format("This is a banner", font="digital")
+        self.banner_count = self.banner.count("\n") + 1
+        print(self.banner)
+
+    def clear_terminal(self):
+        print("\033c", end="")
+        fancy.print_banner()
+
 
 if __name__ == "__main__":
+    # clears the terminal first. this primarily is for executing the code in command prompt. as observed, the vscode terminal has no issues with this to some extent
+    system("cls")
     fancy = Fancy()
-    fancy.printLine("This is a testLine\n")
-    sleep(1)
-    fancy.printLine("This is a another line\n")
-    sleep(1)
-    fancy.printLine("This third line will soon be deleted\n")
-    sleep(1)
-    # fancy.delete_one_line()
-    # fancy.printLine("\nThis a new line")
-    fancy.replace_line("This line replaces the previously deleted line")
+    fancy.print_banner()
+    fancy.printLine("This is a line\n")
+    fancy.printLine("The terminal will be reset")
+    sleep(5)
+    fancy.clear_terminal()
+    fancy.printLine("This is a new line after the reset")
+
+    # fancy.get_name()
+    # fancy.replace_previous_line("This text should replace the previous line")
+    # fancy.printLine(f"Just checking. You are {fancy.name}, right?")
