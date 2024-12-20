1b97fbffc6542aeab9bf07ccad28d8a77af87e0e
franz abarquez <franzdale.abarquez@gmail.com>
2024-12-20 08:43:03 +0800

    continued working on client code

--- simple_tcp_chat/yappn_client.py
+++ simple_tcp_chat/yappn_client.py
@@ -5,3 +5,12 @@ username = input("Choose your user name: ")
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(("127.0.0.1", 55555))
+
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode("ascii")
+        except:
+            print("An error occured in sending the message")
+            client.close()
