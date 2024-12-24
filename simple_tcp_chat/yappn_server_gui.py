2b9176e3070ce1c6e5c22e7bddaa57736ea7104e
franz abarquez <franzdale.abarquez@gmail.com>
2024-12-24 20:49:56 +0800

    continued working on gui code

--- simple_tcp_chat/yappn_server_gui.py
+++ simple_tcp_chat/yappn_server_gui.py
@@ -1,15 +1,24 @@
 import sys
 from PySide6 import QtCore, QtWidgets, QtGui
+import threading
+import socket
 
 
-class appWidget(QtWidgets.QtWidget):
-    pass
+class appWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.mainLayout = QtWidgets.QHBoxLayout(self)
+
+        self.startServer = QtWidgets.QPushButton("Start Server")
+
+        self.mainLayout.addWidget(self.startServer)
+        self.setLayout(self.mainLayout)
 
 
 if __name__ == "__main__":
     server_gui = QtWidgets.QApplication([])
     widget = appWidget()
-    widget = resize(600, 400)
+    widget.resize(600, 400)
     widget.show()
 
     sys.exit(server_gui.exec())
