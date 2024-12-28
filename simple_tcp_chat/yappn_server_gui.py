32b5cf61a60eff0ce28621e914579efea403e3ed
franz abarquez <franzdale.abarquez@gmail.com>
2024-12-28 09:43:33 +0800

    finished working on server gui

--- simple_tcp_chat/yappn_server_gui.py
+++ simple_tcp_chat/yappn_server_gui.py
@@ -34,15 +34,42 @@ def handle(client):
             break
 
 
+def receive():
+    while True:
+        client, address = server.accept()
+        print("Connected with {}".format(str(address)))
+
+        client.send("NICK".encode("ascii"))
+        nickname = client.recv(1024).decode("ascii")
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print("Nickname is {}".format(nickname))
+        broadcast("{} joined!".format(nickname).encode("ascii"))
+        client.send("Connected to server!".encode("ascii"))
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+
 class appWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        self.mainLayout = QtWidgets.QHBoxLayout(self)
+        self.mainLayout = QtWidgets.QVBoxLayout(self)
 
+        self.status_label = QtWidgets.QLabel("Server is not running", self)
+        self.status_label.setAlignment(QtCore.Qt.AlignCenter)
+        self.mainLayout.addWidget(self.status_label)
         self.startServer = QtWidgets.QPushButton("Start Server")
-
+        self.startServer.clicked.connect(self.start_server)
         self.mainLayout.addWidget(self.startServer)
         self.setLayout(self.mainLayout)
+
+    def start_server(self):
+        self.server_thread = threading.Thread(target=receive)
+        self.server_thread.daemon = True
+        self.server_thread.start()
+        self.status_label.setText("Server is running")
 
 
 if __name__ == "__main__":
