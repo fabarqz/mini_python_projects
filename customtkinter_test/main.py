0f0c2858e1e05f0f2597e7f96bf44fd76203b261
franz abarquez <franzdale.abarquez@gmail.com>
2024-12-30 22:48:46 +0800

    continued working on code

--- customtkinter_test/main.py
+++ customtkinter_test/main.py
@@ -5,3 +5,16 @@ customtkinter.set_default_color_theme("dark-blue")
 
 root = customtkinter.CTk()
 root.geometry("500x350")
+
+
+def login():
+    print("Test")
+
+
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=20, padx=2, fill="both", expand=True)
+
+label = customtkinter.CTkLabel(master=frame, text="Login System", font=("Roboto", 24))
+label.pack(pad=12, padx=10)
+
+entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
