bea72a4f04946e157111129fc231f4983e6bcc00
franz abarquez <franzdale.abarquez@gmail.com>
2025-01-01 09:26:05 +0800

    added a basic template code for empty window using customtkinter

---
+++ customtkinter_test/main_test_a.py
@@ -0,0 +1,10 @@
+import customtkinter
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("green")
+
+root = customtkinter.CTk()
+root.geometry("800x600")
+root.title("Basic Window")
+
+root.mainloop()
