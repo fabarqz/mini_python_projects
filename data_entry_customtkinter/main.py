224a8275fd42e6a137333762cad958f30eb74cb7
franz abarquez <franzdale.abarquez@gmail.com>
2025-01-04 03:46:20 +0800

    continued working on code

--- data_entry_customtkinter/main.py
+++ data_entry_customtkinter/main.py
@@ -1,19 +1,21 @@
-import customtkinter
+import customtkinter as ctk
 
-customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("green")
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("green")
 
-root = customtkinter.CTk()
+root = ctk.CTk()
 root.geometry("800x600")
 root.title("Data Entry Form")
 
-frame = customtkinter.CTkFrame(root)
+frame_label = ctk.CTkLabel(root, text="User Information")
+frame_label.grid(row=0, column=0, padx=10, pady=(10, 0))
 
+frame = ctk.CTkFrame(root, width=200, height=150, corner_radius=10)
+frame.grid(row=1, column=0, padx=10, pady=(0, 10))
 
-user_info_frame = customtkinter.CTkLabel(master=frame, text="User Information")
-user_info_frame.grid(row=0, column=0)
+first_name_label = ctk.CTkLabel(frame, text="First Name")
+first_name_label.grid(row=0, column=0, padx=10, pady=(0, 10))
 
-first_name_label = customtkinter.CTkLabel(master=frame, text="First Name")
-user_info_frame.grid(row=0, column=0)
-frame.pack()
+last_name_label = ctk.CTkLabel(frame, text="Last Name")
+last_name_label.grid(row=0, column=1, padx=10, pady=(0, 10))
 root.mainloop()
