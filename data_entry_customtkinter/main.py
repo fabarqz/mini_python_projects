cdb1e5ffb7e6cbf347cec13e66832c385ed2b10f
franz abarquez <franzdale.abarquez@gmail.com>
2025-01-07 07:42:31 +0800

    continued working on code

--- data_entry_customtkinter/main.py
+++ data_entry_customtkinter/main.py
@@ -14,14 +14,20 @@ frame = ctk.CTkFrame(root, width=200, height=150, corner_radius=10)
 frame.grid(row=1, column=0, padx=10, pady=(0, 10))
 
 first_name_label = ctk.CTkLabel(frame, text="First Name")
-first_name_label.grid(row=0, column=0, padx=10, pady=(0, 10))
+first_name_label.grid(row=0, column=1, padx=10, pady=(0, 10))
 
 last_name_label = ctk.CTkLabel(frame, text="Last Name")
-last_name_label.grid(row=0, column=1, padx=10, pady=(0, 10))
+last_name_label.grid(row=0, column=0, padx=10, pady=(0, 10))
+
+middle_initial_label = ctk.CTkLabel(frame, text="Middle Initial")
+middle_initial_label.grid(row=0, column=2, padx=10, pady=(0, 10))
 
 first_name_entry = ctk.CTkEntry(frame)
-first_name_entry.grid(row=1, column=0, padx=10, pady=(0, 10))
+first_name_entry.grid(row=1, column=1, padx=10, pady=(0, 10))
 
 last_name_entry = ctk.CTkEntry(frame)
-last_name_entry.grid(row=1, column=1, padx=10, pady=(0, 10))
+last_name_entry.grid(row=1, column=0, padx=10, pady=(0, 10))
+
+middle_initial_entry = ctk.CTkEntry(frame)
+middle_initial_entry.grid(row=1, column=2, padx=10, pady=(0, 10))
 root.mainloop()
