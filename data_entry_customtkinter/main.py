9692ee0240c5e815a465692e71a65cd28220a66d
franz abarquez <franzdale.abarquez@gmail.com>
2025-01-08 08:10:23 +0800

    continued working on code. added label and combobox for titles of the user

--- data_entry_customtkinter/main.py
+++ data_entry_customtkinter/main.py
@@ -30,4 +30,12 @@ last_name_entry.grid(row=1, column=0, padx=10, pady=(0, 10))
 
 middle_initial_entry = ctk.CTkEntry(frame)
 middle_initial_entry.grid(row=1, column=2, padx=10, pady=(0, 10))
+
+title_label = ctk.CTkLabel(frame, text="Title")
+titles = ["", "Mr.", "Ms.", "Dr.", "Engr."]
+title_combobox = ctk.CTkComboBox(frame, values=titles)
+
+title_label.grid(row=0, column=3, padx=10, pady=(0, 10))
+title_combobox.grid(row=1, column=3, padx=10, pady=(0, 10))
+
 root.mainloop()
